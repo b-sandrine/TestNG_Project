369ea6f0fc5762f3a13bf0bc58cada00badda188
b-sandrine <sanrdushimimana@gmail.com>
2022-11-16 21:16:27 +0200

    create services

--- src/main/java/backend/meme/memeGenerator/service/MemeService.java
+++ src/main/java/backend/meme/memeGenerator/service/MemeService.java
@@ -1,5 +1,33 @@
 package backend.meme.memeGenerator.service;
 
-public class MemeService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import backend.meme.memeGenerator.model.Meme;
+import backend.meme.memeGenerator.repository.MemeItemRepository;
+
+@Service
+public class MemeService {
+	@Autowired
+	MemeItemRepository memeRepository;
+	
+	public Meme save(Meme item) {
+		return memeRepository.save(item);
+	}
+	
+	public List<Meme> findAll() {
+		return memeRepository.findAll();
+	}
+	
+	public Optional<Meme> findById(int id) {
+		return memeRepository.findById(id);
+	}
+	
+	public void deleteById(int id) {
+		memeRepository.deleteById(id);
+	}
+	
 }
