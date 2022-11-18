abc61c533e4020a8bdefcfabbba9528bab948ad2
b-sandrine <sanrdushimimana@gmail.com>
2022-11-18 11:44:00 +0200

    Add service features

--- src/main/java/backend/meme/memeGenerator/service/MemeService.java
+++ src/main/java/backend/meme/memeGenerator/service/MemeService.java
@@ -1,5 +1,25 @@
 package backend.meme.memeGenerator.service;
 
-public class MemeService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import backend.meme.memeGenerator.model.Meme;
+import backend.meme.memeGenerator.repository.MemeItemRepository;
+
+public class MemeService {
+	@Autowired
+	MemeItemRepository memeRepository;
+	
+	public Meme saveItem(Meme item) {
+		return memeRepository.save(item);
+	}
+	
+	public List<Meme> findMemes() {
+		return memeRepository.findAll();
+	}
+	
+	public void deleteMemeById(int id) {
+		memeRepository.deleteById(id);
+	}
 }
