38b931a1ec56583f0a5ea2f49dba1c0662aa4cb2
b-sandrine <sanrdushimimana@gmail.com>
2022-11-27 13:54:09 +0200

    Clean codes in repository

--- src/main/java/backend/meme/memeGenerator/repository/MemeItemRepository.java
+++ src/main/java/backend/meme/memeGenerator/repository/MemeItemRepository.java
@@ -1,8 +1,5 @@
 package backend.meme.memeGenerator.repository;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +7,5 @@ import backend.meme.memeGenerator.model.Meme;
 
 @Repository 
 public interface MemeItemRepository extends JpaRepository<Meme, Integer> {
-	long countItems();
-	Optional<Meme> findById(int id);
-	List<Meme> findAll();
+	
 }
